dba32ce8592c7058e0c7773f069a940f6d74bcac
illuminxry <secret.ian0926@gmail.com>
2023-11-12 22:19:38 +0800

    corrected

--- controller/index.js
+++ controller/index.js
@@ -1,33 +1,32 @@
+require('dotenv').config();
+const OpenAI = require('openai').OpenAI;
+const openai = new OpenAI;
+
+exports.getIndexPage = (req, res) => {
+    res.render('index', { chatResponse: null });
+};
+
 exports.postChatResponse = async (req, res) => {
-        try {
-            const userInput = req.body.userInput || 'Hi! How can I help you today?';
-    
-            const response = await openai.chat.completions.create({
-                model: 'gpt-3.5-turbo',
-                messages: [
-                    {
-                        role: 'system',
-                        content: 'You DAZSMA.AI, a kind and friendly chatbot.'
-                    },
-                    {
-                        role: 'user',
-                        content: userInput
-                    }
-                ],
-                max_tokens: 64, // Set the maximum number of tokens in the response
-            });
-    
-            const truncatedResponse = response.choices[0].message.content;
-    
-            // Ensure the response has at most 64 tokens
-            const tokens = truncatedResponse.split(' ');
-            if (tokens.length > 64) {
-                truncatedResponse = tokens.slice(0, 64).join(' ') + '...';
-            }
-    
-            res.render('index', { chatResponse: truncatedResponse });
-        } catch (error) {
-            console.error('Error fetching chat response:', error);
-            res.status(500).send('Internal Server Error');
-        }
-    };
+    try {
+        const userInput = req.body.userInput || 'Hi! How can I help you today?';
+
+        const response = await openai.chat.completions.create({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'system',
+                    content: 'You are DAZSMA.AI, a kind and friendly chatbot.'
+                },
+                {
+                    role: 'user',
+                    content: userInput
+                }
+            ],
+        });
+
+        res.render('index', { chatResponse: response.choices[0].message.content });
+    } catch (error) {
+        console.error('Error fetching chat response:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
